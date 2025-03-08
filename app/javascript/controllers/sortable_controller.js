40ccb2cd1160107ccc2383f18b42ac542edfd792
Naiyyar <naiyyar.abbas512013@gmail.com>
2025-03-08 08:37:30 +0530

    group specific drop fix, some css changes

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -3,10 +3,21 @@ import Sortable from 'sortablejs'
 
 // Connects to data-controller="sortable"
 export default class extends Controller {
+  static values = { group: String } 
+  
   connect() {
-    console.log(this.element)
     new Sortable(this.element, {
-
+      group: this.groupValue,
+      onEnd: this.onEnd.bind(this),
+      onStart: this.onStart.bind(this)
     })
+  }
+
+  onEnd(ev){
+    console.log(ev)
+  }
+
+  onStart(){
+    this.element.style.cursor = 'grab'
   }
 }
