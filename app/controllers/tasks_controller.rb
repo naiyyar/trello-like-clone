d271719878131dc4831e708f4632ec0e3638bc1d
Naiyyar <naiyyar.abbas512013@gmail.com>
2025-03-10 22:11:49 +0530

    inplace task editing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,11 +54,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.turbo_stream do
-          turbo_stream.replace "edit_task_#{@task.id}" do
-            render partial: "tasks/task", locals: { task: @task, list: @task.list }
-          end
-        end
+        format.turbo_stream
         format.html { redirect_to @task, notice: "Task was successfully updated." }
         format.json { render :show, status: :ok, location: @task }
       else
