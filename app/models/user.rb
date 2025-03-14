ec6c4a343170c4845a1d73c30e334499773bc0db
Naiyyar <naiyyar.abbas512013@gmail.com>
2025-03-14 20:13:16 +0530

    displaying invited boards

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :boards, dependent: :destroy
+  has_many :board_invitations, dependent: :destroy
   has_many :invited_boards, through: :board_invitations, source: :board
 
   def name
