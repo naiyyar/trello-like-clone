ff7663b91ee90d55efbe69e86ff1b4c7d1045cde
Naiyyar <naiyyar.abbas512013@gmail.com>
2025-03-14 13:45:35 +0530

    adding status using enums

--- app/models/board_invitation.rb
+++ app/models/board_invitation.rb
@@ -1,5 +1,6 @@
 class BoardInvitation < ApplicationRecord
-  enum status: { pending: 0, accepted: 1, declined: 2 }
+  enum :status, { pending: 0, accepted: 1, rejected: 2 }
+
   belongs_to :board
   belongs_to :user
 
